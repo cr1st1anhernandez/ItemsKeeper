f156f29804458b4a9b283c6c983d877d58bec6e8
Cristian Hernandez Ramirez <cristian0402218@gmail.com>
2024-08-06 19:00:53 -0600

    🚀 Add fly io settings

---
+++ Backend/src/main/java/com/cr1st1an/itemskeeper/backend/persistence/respositories/CommentRepository.java
@@ -0,0 +1,2 @@
+package com.cr1st1an.itemskeeper.backend.persistence.respositories;public interface CommentRepository {
+}
---
+++ Backend/src/main/java/com/cr1st1an/itemskeeper/backend/services/IItemService.java
@@ -0,0 +1,2 @@
+package com.cr1st1an.itemskeeper.backend.services;public interface IItemService {
+}
